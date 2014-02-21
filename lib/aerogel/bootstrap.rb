9143426e4b7823615e4bf830ca2c1620d68a5b8e
Alex Kukushkin <alex@kukushk.in>
2014-02-21 14:53:25 +0200

    Moved VERSION into a separate file
    - fixes gemspec dependency on installed gems

--- lib/aerogel/bootstrap.rb
+++ lib/aerogel/bootstrap.rb
@@ -1,11 +1,8 @@
 require 'aerogel/core'
+require 'aerogel/bootstrap/version'
 require 'bootstrap-sass'
 
 module Aerogel
-  module AerogelBootstrap
-    VERSION = "0.0.2"
-  end
-
   # Finally, register module's root folder
   # register_path File.join( File.dirname(__FILE__), '..', '..' )
   register_path Bootstrap.assets_path, :assets
---
+++ lib/aerogel/bootstrap/version.rb
@@ -0,0 +1,5 @@
+module Aerogel
+  module AerogelBootstrap
+    VERSION = "0.0.2"
+  end # module AerogelBootstrap
+end # module Aerogel
