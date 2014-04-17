2f8b1dc3bc29161527c02902707acad96df9953e
Alex Kukushkin <alex@kukushk.in>
2014-04-17 21:38:48 +0300

    Imported Bootstrap scss sources into this, as original boostrap-sass messes with Sass load paths

--- lib/aerogel/bootstrap.rb
+++ lib/aerogel/bootstrap.rb
@@ -1,9 +1,10 @@
 require 'aerogel/core'
 require 'aerogel/bootstrap/version'
-require 'bootstrap-sass'
+# require 'bootstrap-sass'
 
 module Aerogel
   # Finally, register module's root folder
-  # register_path File.join( File.dirname(__FILE__), '..', '..' )
-  register_path Bootstrap.assets_path, :assets
+  register_path File.join( File.dirname(__FILE__), '..', '..' )
+  # boostrap_assets_path = File.join( File.dirname(__FILE__), '..', '..', 'assets' )
+  # register_path boostrap_assets_path, :assets
 end
